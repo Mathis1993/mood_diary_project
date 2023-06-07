b1bfcc1ceec28e5d20e0bf98f03e0ab078a8f11b
Mathis1993 <m_eric01@uni-muenster.de>
2023-06-07 19:06:13 +0200

    Add crud views (get, get many, create, update, delete)

--- mood_diary/diaries/views.py
+++ mood_diary/diaries/views.py
@@ -1,1 +1,68 @@
-# Create your views here.
+from core.views import AuthenticatedClientRoleMixin
+from diaries.forms import MoodDiaryEntryForm
+from diaries.models import MoodDiary, MoodDiaryEntry
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import DeleteView, ListView, UpdateView
+from django.views.generic.detail import DetailView
+
+
+class MoodDiaryEntryDetailView(DetailView):
+    model = MoodDiaryEntry
+    template_name = "diaries/mood_diary_entry_get.html"
+    context_object_name = "entry"
+
+
+class MoodDiaryEntryListView(AuthenticatedClientRoleMixin, ListView):
+    model = MoodDiaryEntry
+    template_name = "diaries/mood_diary_entries_list.html"
+    context_object_name = "entries"
+    paginate_by = 10
+
+    def get_queryset(self):
+        client_id = self.request.user.client.id
+        mood_diary, _ = MoodDiary.objects.get_or_create(client_id=client_id)
+        return mood_diary.entries.all().order_by("-created_at")
+
+
+class CreateMoodDiaryEntryView(AuthenticatedClientRoleMixin, View):
+    form_class = MoodDiaryEntryForm
+    template_name = "diaries/mood_diary_entry_create.html"
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.mood_diary = request.user.client.mood_diary
+            entry.save()
+            return redirect("diaries:list_mood_diary_entries")
+        return render(request, self.template_name, {"form": form})
+
+
+class MoodDiaryEntryUpdateView(UpdateView):
+    model = MoodDiaryEntry
+    template_name = "diaries/mood_diary_entry_update.html"
+    fields = [
+        "start_time",
+        "end_time",
+        "mood",
+        "emotion",
+        "mood_and_emotion_info",
+        "activity",
+        "strain",
+        "strain_area",
+        "strain_info",
+    ]
+    success_url = reverse_lazy("diaries:list_mood_diary_entries")
+
+
+class MoodDiaryEntryDeleteView(DeleteView):
+    model = MoodDiaryEntry
+    template_name = "diaries/mood_diary_entry_delete.html"
+    context_object_name = "entry"
+    success_url = reverse_lazy("diaries:list_mood_diary_entries")
