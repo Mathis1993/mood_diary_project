c57cb131f36ed37356af7047b0c0b2cefb1ce6b8
Mathis1993 <m_eric01@uni-muenster.de>
2023-06-07 19:02:50 +0200

    Add mixins for authenticated counselor/client role checks

--- mood_diary/core/views.py
+++ mood_diary/core/views.py
@@ -1,1 +1,15 @@
-# ToDo(ME-06.06.23): Create basic mixins here
+from django.contrib.auth.mixins import AccessMixin
+
+
+class AuthenticatedCounselorRoleMixin(AccessMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not (user := request.user).is_authenticated or not user.is_counselor():
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+
+class AuthenticatedClientRoleMixin(AccessMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not (user := request.user).is_authenticated or not user.is_client():
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
