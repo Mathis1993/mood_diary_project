2fb2558fe5285696bdf31760a8ed65f8466ca549
Mathis1993 <m_eric01@uni-muenster.de>
2023-06-07 19:06:57 +0200

    Add diaries app urls

---
+++ mood_diary/diaries/urls.py
@@ -0,0 +1,32 @@
+from diaries import views
+from django.urls import path
+
+app_name = "diaries"
+
+urlpatterns = [
+    path(
+        "mood_diary_entry/<int:pk>/get/",
+        views.MoodDiaryEntryDetailView.as_view(),
+        name="get_mood_diary_entry",
+    ),
+    path(
+        "mood_diary_entry/get_all/",
+        views.MoodDiaryEntryListView.as_view(),
+        name="list_mood_diary_entries",
+    ),
+    path(
+        "mood_diary_entry/create/",
+        views.CreateMoodDiaryEntryView.as_view(),
+        name="create_mood_diary_entry",
+    ),
+    path(
+        "mood_diary_entry/<int:pk>/update/",
+        views.MoodDiaryEntryUpdateView.as_view(),
+        name="update_mood_diary_entry",
+    ),
+    path(
+        "mood_diary_entry/<int:pk>/delete/",
+        views.MoodDiaryEntryDeleteView.as_view(),
+        name="delete_mood_diary_entry",
+    ),
+]
