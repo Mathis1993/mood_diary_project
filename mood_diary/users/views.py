ebe29c465f5ead8399133bcafaf40bdecf1ca484
Mathis1993 <m_eric01@uni-muenster.de>
2023-06-12 13:59:35 +0200

    Extend index view with redirects corresponding to the user role

--- mood_diary/users/views.py
+++ mood_diary/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import PasswordChangeView
@@ -5,22 +6,28 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 
-
-@login_required(login_url="users:login")
-def logout_confirmation(request: HttpRequest) -> HttpResponse:
-    return render(request, "users/logout.html")
+User = get_user_model()
 
 
 @login_required(login_url="users:login")
 def index(request: HttpRequest) -> HttpResponse:
     user = request.user
-    print(user.last_login)
-    print(user.is_superuser)
     if not (user.first_login_completed or user.is_superuser):
-        print("here")
         return redirect("users:change_password")
-    else:
-        return HttpResponse("Hello, world.")
+
+    if user.role == User.Role.ADMIN:
+        return redirect("admin:index")
+
+    if user.role == User.Role.COUNSELOR:
+        return redirect("dashboards:dashboard_counselor")
+
+    if user.role == User.Role.CLIENT:
+        return redirect("dashboards:dashboard_client")
+
+
+@login_required(login_url="users:login")
+def logout_confirmation(request: HttpRequest) -> HttpResponse:
+    return render(request, "users/logout.html")
 
 
 class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
