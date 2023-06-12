f99da0ac2ee95c406d5ddfabfe639202298ddb52
Mathis1993 <m_eric01@uni-muenster.de>
2023-06-12 11:33:13 +0200

    Add validation for fields depending on antoher

--- mood_diary/diaries/forms.py
+++ mood_diary/diaries/forms.py
@@ -6,6 +6,7 @@ class MoodDiaryEntryForm(forms.ModelForm):
     class Meta:
         model = MoodDiaryEntry
         fields = [
+            "date",
             "start_time",
             "end_time",
             "mood",
@@ -16,3 +17,17 @@ class MoodDiaryEntryForm(forms.ModelForm):
             "strain_area",
             "strain_info",
         ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        strain = cleaned_data.get("strain")
+        if strain:
+            return cleaned_data
+
+        strain_area = cleaned_data.get("strain_area")
+        if strain_area:
+            self.add_error("strain_area", "Please set a strain when choosing a strain area.")
+
+        strain_info = cleaned_data.get("strain_info")
+        if strain_info:
+            self.add_error("strain_info", "Please set a strain when providing strain info.")
