b7a8286bc0565323059e62a82002f5c2611da1ab
Mathis1993 <m_eric01@uni-muenster.de>
2023-06-12 18:55:58 +0200

    Use plural in routes for consistency

--- mood_diary/diaries/urls.py
+++ mood_diary/diaries/urls.py
@@ -5,27 +5,27 @@ app_name = "diaries"
 
 urlpatterns = [
     path(
-        "mood_diary_entry/<int:pk>/get/",
+        "mood_diary_entries/<int:pk>/get/",
         views.MoodDiaryEntryDetailView.as_view(),
         name="get_mood_diary_entry",
     ),
     path(
-        "mood_diary_entry/get_all/",
+        "mood_diary_entries/get_all/",
         views.MoodDiaryEntryListView.as_view(),
         name="list_mood_diary_entries",
     ),
     path(
-        "mood_diary_entry/create/",
+        "mood_diary_entries/create/",
         views.CreateMoodDiaryEntryView.as_view(),
         name="create_mood_diary_entry",
     ),
     path(
-        "mood_diary_entry/<int:pk>/update/",
+        "mood_diary_entries/<int:pk>/update/",
         views.MoodDiaryEntryUpdateView.as_view(),
         name="update_mood_diary_entry",
     ),
     path(
-        "mood_diary_entry/<int:pk>/delete/",
+        "mood_diary_entries/<int:pk>/delete/",
         views.MoodDiaryEntryDeleteView.as_view(),
         name="delete_mood_diary_entry",
     ),
