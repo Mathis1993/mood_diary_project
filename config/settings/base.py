6449a4f4a02eb90acefddce29d16d4b3799fec7e
Mathis1993 <m_eric01@uni-muenster.de>
2023-06-24 15:24:19 +0200

    #2 Add correct icon and splash screen paths

--- config/settings/base.py
+++ config/settings/base.py
@@ -147,8 +147,8 @@ DATABASES = {
 }
 
 # PWA
-PWA_APP_NAME = "Mood Diary App"
-PWA_APP_DESCRIPTION = "Mood Diary App"
+PWA_APP_NAME = "Mood Diary"
+PWA_APP_DESCRIPTION = "Mood Diary"
 PWA_APP_THEME_COLOR = "#0A0302"
 PWA_APP_BACKGROUND_COLOR = "#ffffff"
 PWA_APP_DISPLAY = "standalone"
@@ -156,11 +156,143 @@ PWA_APP_SCOPE = "/"
 PWA_APP_ORIENTATION = "any"
 PWA_APP_START_URL = "/"
 PWA_APP_STATUS_BAR_COLOR = "default"
-PWA_APP_ICONS = [{"src": "/static/images/my_app_icon.png", "sizes": "160x160"}]
-PWA_APP_ICONS_APPLE = [{"src": "/static/images/my_apple_icon.png", "sizes": "160x160"}]
+PWA_APP_ICONS = [{"src": "/static/images/icons/android-chrome-512x512.png", "sizes": "160x160"}]
+PWA_APP_ICONS_APPLE = [{"src": "/static/images/icons/apple-touch-icon.png", "sizes": "160x160"}]
 PWA_APP_SPLASH_SCREEN = [
     {
-        "src": "/static/images/icons/splash-640x1136.png",
-        "media": "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",  # noqa E501
-    }
+        "src": "static/images/splash_screens/iPhone_14_Pro_Max_landscape.png",
+        "media": "screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",  # noqa: E501  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_14_Pro_landscape.png",
+        "media": "screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_landscape.png",  # noqa: E501
+        "media": "screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_landscape.png",  # noqa: E501
+        "media": "screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_landscape.png",  # noqa: E501
+        "media": "screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_landscape.png",
+        "media": "screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_11__iPhone_XR_landscape.png",
+        "media": "screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png",  # noqa: E501
+        "media": "screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_landscape.png",  # noqa: E501
+        "media": "screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_landscape.png",  # noqa: E501
+        "media": "screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/12.9__iPad_Pro_landscape.png",
+        "media": "screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/11__iPad_Pro__10.5__iPad_Pro_landscape.png",
+        "media": "screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/10.9__iPad_Air_landscape.png",
+        "media": "screen and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/10.5__iPad_Air_landscape.png",
+        "media": "screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/10.2__iPad_landscape.png",
+        "media": "screen and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_landscape.png",  # noqa: E501
+        "media": "screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/8.3__iPad_Mini_landscape.png",
+        "media": "screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_14_Pro_Max_portrait.png",
+        "media": "screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_14_Pro_portrait.png",
+        "media": "screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png",  # noqa: E501
+        "media": "screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png",  # noqa: E501
+        "media": "screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png",  # noqa: E501
+        "media": "screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png",
+        "media": "screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_11__iPhone_XR_portrait.png",
+        "media": "screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png",  # noqa: E501
+        "media": "screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png",  # noqa: E501
+        "media": "screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png",  # noqa: E501
+        "media": "screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/12.9__iPad_Pro_portrait.png",
+        "media": "screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png",
+        "media": "screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/10.9__iPad_Air_portrait.png",
+        "media": "screen and (device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/10.5__iPad_Air_portrait.png",
+        "media": "screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/10.2__iPad_portrait.png",
+        "media": "screen and (device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png",  # noqa: E501
+        "media": "screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
+    {
+        "src": "static/images/splash_screens/8.3__iPad_Mini_portrait.png",
+        "media": "screen and (device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",  # noqa: E501
+    },
 ]
