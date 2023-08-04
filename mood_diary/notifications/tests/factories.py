cd7a4c29a651b24f82c3c46fde8ae91eef902431
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-04 18:25:00 +0200

    #6 Fix model field (we want to target the client specifically)

--- mood_diary/notifications/tests/factories.py
+++ mood_diary/notifications/tests/factories.py
@@ -6,6 +6,6 @@ class NotificationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Notification
 
-    user = factory.SubFactory("users.tests.factories.UserFactory")
+    client = factory.SubFactory("clients.tests.factories.ClientFactory")
     message = factory.Faker("text", max_nb_chars=200)
     viewed = factory.fuzzy.FuzzyChoice([True, False])
