265f342b8a5ad64908485913b8f0c64ec674fb3d
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-04 18:33:51 +0200

    #7 Extend tests - test RelaxingActivityRule

--- mood_diary/rules/tests/test_rules.py
+++ mood_diary/rules/tests/test_rules.py
@@ -2,12 +2,12 @@ import time
 
 import pytest
 from clients.tests.factories import ClientFactory
-from diaries.models import MoodDiaryEntry
+from diaries.models import ActivityCategory, MoodDiaryEntry
 from diaries.tests.factories import MoodDiaryEntryFactory
 from django.utils import timezone
 from notifications.models import Notification
 from rules.models import RuleTriggeredLog
-from rules.rules import ActivityWithPeakMoodRule, BaseRule
+from rules.rules import ActivityWithPeakMoodRule, BaseRule, RelaxingActivityRule
 from rules.tests.factories import RuleFactory
 
 
@@ -95,6 +95,45 @@ def test_activity_with_peak_mood_rule():
     new_target_entry = MoodDiaryEntryFactory.create(mood_diary__client=client, mood__value=3)
     new_timestamp = timezone.now()
     rule = ActivityWithPeakMoodRule(client_id=client.id, requested_at=new_timestamp)
+    assert rule.get_mood_diary_entries().first() == new_target_entry
+    assert not Notification.objects.exists()
+    assert not RuleTriggeredLog.objects.exists()
+    rule.evaluate()
+    assert Notification.objects.count() == 1
+    assert RuleTriggeredLog.objects.count() == 1
+
+    rule_db.subscribed_clients.remove(client)
+    assert rule.client_subscribed() is False
+    rule.evaluate()  # no effect
+    assert Notification.objects.count() == 1
+    assert RuleTriggeredLog.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_relaxing_activity_mood_rule():
+    client = ClientFactory.create()
+    rule_db = RuleFactory.create(title="Relaxing activity")
+    rule_db.subscribed_clients.add(client)
+    MoodDiaryEntryFactory.create(mood_diary__client=client, activity__category__value="Work")
+    time.sleep(0.1)
+    target_entry = MoodDiaryEntryFactory.create(
+        mood_diary__client=client, activity__category__value="Social"
+    )
+    timestamp = timezone.now()
+    time.sleep(0.1)
+    MoodDiaryEntryFactory.create(mood_diary__client=client, activity__category__value="Studies")
+    rule = RelaxingActivityRule(client_id=client.id, requested_at=timestamp)
+    assert rule.triggering_allowed() is True
+    entries = rule.get_mood_diary_entries()
+    assert entries.first().id == target_entry.id
+    assert rule.evaluate_preconditions() is False
+
+    time.sleep(0.1)
+    new_target_entry = MoodDiaryEntryFactory.create(
+        mood_diary__client=client, activity__category__value=ActivityCategory.relaxing_value
+    )
+    new_timestamp = timezone.now()
+    rule = RelaxingActivityRule(client_id=client.id, requested_at=new_timestamp)
     assert rule.get_mood_diary_entries().first() == new_target_entry
     assert not Notification.objects.exists()
     assert not RuleTriggeredLog.objects.exists()
