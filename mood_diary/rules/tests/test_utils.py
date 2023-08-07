c73f11e30eef130c2f5482bad250becb1d00da58
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-07 10:13:01 +0200

    #7 Update helper function to accept date for which the beginning of the week is calculated

--- mood_diary/rules/tests/test_utils.py
+++ mood_diary/rules/tests/test_utils.py
@@ -1,18 +1,21 @@
 from datetime import datetime
 
 from django.utils import timezone
-from rules.utils import get_beginning_of_current_week
+from rules.utils import get_beginning_of_week
 
 
 def test_get_beginning_of_current_week(freezer):
     freezer.move_to("2023-09-27 00:00:00")
-    beginning_of_week = get_beginning_of_current_week()
+    beginning_of_week = get_beginning_of_week()
     assert beginning_of_week == datetime(2023, 9, 25, 0, 0, 0, tzinfo=timezone.utc)
 
     freezer.move_to("2023-10-01 23:59:59")
-    beginning_of_week = get_beginning_of_current_week()
+    beginning_of_week = get_beginning_of_week()
     assert beginning_of_week == datetime(2023, 9, 25, 0, 0, 0, tzinfo=timezone.utc)
 
     freezer.move_to("2023-10-02 00:00:00")
-    beginning_of_week = get_beginning_of_current_week()
+    beginning_of_week = get_beginning_of_week()
     assert beginning_of_week == datetime(2023, 10, 2, 0, 0, 0, tzinfo=timezone.utc)
+
+    beginning_of_week = get_beginning_of_week(datetime(2023, 8, 30, 0, 0, 0, tzinfo=timezone.utc))
+    assert beginning_of_week == datetime(2023, 8, 28, 0, 0, 0, tzinfo=timezone.utc)
--- mood_diary/rules/utils.py
+++ mood_diary/rules/utils.py
@@ -3,10 +3,10 @@ from datetime import timedelta
 from django.utils import timezone
 
 
-def get_beginning_of_current_week():
+def get_beginning_of_week(today: timezone.datetime = None) -> timezone.datetime:
     """
     Returns timestamp of the Monday of the current week at 00:00:00.
     """
-    today = timezone.now()
+    today = today or timezone.now()
     monday = today - timedelta(days=today.weekday())
     return monday.replace(hour=0, minute=0, second=0, microsecond=0)
