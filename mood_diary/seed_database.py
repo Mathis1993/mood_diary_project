3a57d470bc823bf3eafe476c34e964035fc4f41a
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-08 10:14:25 +0200

    #8 Update seeding with respect to rules

--- mood_diary/seed_database.py
+++ mood_diary/seed_database.py
@@ -7,6 +7,9 @@ from diaries.tests.factories import (
 )
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from rules.models import Rule
+from rules.rules import EVENT_BASED_RULES, TIME_BASED_RULES
+from rules.tests.factories import RuleFactory
 from users.tests.factories import UserFactory
 
 User = get_user_model()
@@ -16,14 +19,27 @@ def seed_database():
     User.objects.create_superuser(
         email="mathis@mood-diary.de", password=settings.TEST_USER_PASSWORD
     )
-    counselor = UserFactory.create(role=User.Role.COUNSELOR)
-    client_user = UserFactory.create(role=User.Role.CLIENT)
-    client = ClientFactory.create(user=client_user, counselor=counselor)
+
+    [
+        RuleFactory.create(title=rule.rule_title, evaluation=Rule.Evaluation.EVENT_BASED)
+        for rule in EVENT_BASED_RULES
+    ]
+    [
+        RuleFactory.create(title=rule.rule_title, evaluation=Rule.Evaluation.TIME_BASED)
+        for rule in TIME_BASED_RULES
+    ]
     [
         ActivityFactory.create(category__value=category, value=activity)
         for category, activities in ACTIVITIES.items()
         for activity in activities
     ]
+
+    counselor = UserFactory.create(role=User.Role.COUNSELOR)
+    client_user = UserFactory.create(role=User.Role.CLIENT)
+    client = ClientFactory.create(
+        user=client_user, counselor=counselor, subscribed_rules=Rule.objects.all()
+    )
+
     mood_diary = MoodDiaryFactory.create(client=client)
     for _ in range(25):
         MoodDiaryEntryFactory.create(mood_diary=mood_diary)
