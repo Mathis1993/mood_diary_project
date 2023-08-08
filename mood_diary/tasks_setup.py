afa0ee0b96e2cb1510fb2e9c51afdacdb91c568e
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-08 10:18:11 +0200

    #8 Register periodic task

--- mood_diary/tasks_setup.py
+++ mood_diary/tasks_setup.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery("mood_diary", broker=os.getenv("CELERY_BROKER_URL"))
 
@@ -12,3 +13,12 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+# Register periodic tasks
+celery_beat_schedule = {
+    "Evaluation of time-based rules": {
+        "task": "diaries.tasks.task_time_based_rules_init",
+        "schedule": crontab(hour="6", minute="0"),
+    },
+}
+app.conf.beat_schedule = celery_beat_schedule
