7d58b67a5b667a768d033babb32772a4f8569a77
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-10 20:20:19 +0200

    #11 Add from_email setting for tests

--- config/settings/test.py
+++ config/settings/test.py
@@ -5,3 +5,5 @@ TEST_USER_PASSWORD = "password1"
 # python manage.py runserver 0.0.0.0:8000 and local machine's host name in ALLOWED_HOSTS
 # to connect from phone via host-name:8000/...
 ALLOWED_HOSTS = ["baby-yoda.local", "localhost"]
+
+FROM_EMAIL = "test@test.de"
