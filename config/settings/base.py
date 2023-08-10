c2158243a921bbf93715c46a0c9822fc6a680045
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-10 20:25:27 +0200

    #11 Put some sensitive setting values into env variables

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,23 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent / "mood_diary"
 sys.path.append(str(BASE_DIR))
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = BASE_DIR / "static_root"
-STATICFILES_DIRS = [BASE_DIR / "mood_diary/static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# ToDo(ME-06.06.23): Generate and hide real secret key
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ry1=u862==y3t)lm!kw53*06ug)50(sz$8og2=g6a-z-xw=+w7"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-# ToDo(ME-10.08.23): Not in production
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ToDo(ME-10.08.23): As env variable
-ALLOWED_HOSTS = ["mood-diary.jabba.crossbow.de"]
-CSRF_TRUSTED_ORIGINS = ["https://mood-diary.jabba.crossbow.de"]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
 
 
 # Application definition
