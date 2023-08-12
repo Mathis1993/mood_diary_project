5a9ae69e4e303991f6a8cd65cac4f4f24a6f8abe
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-12 12:48:49 +0200

    #12 Increase gunicorn server log output

--- scripts/startup_django.sh
+++ scripts/startup_django.sh
@@ -2,4 +2,5 @@
 
 python manage.py collectstatic --noinput
 
-gunicorn --bind 0.0.0.0:8000 --worker-class=gthread config.wsgi
+# Write logs to stdout/stderr
+gunicorn --bind 0.0.0.0:8000 --worker-class=gthread --log-level=debug --access-logfile=- --error-logfile=- config.wsgi
