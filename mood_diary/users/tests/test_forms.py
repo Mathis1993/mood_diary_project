762ad6c03aa65811841c9b9486ca2cc17caedb71
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-15 19:44:24 +0200

    #15 Test new form

--- mood_diary/users/tests/test_forms.py
+++ mood_diary/users/tests/test_forms.py
@@ -1,5 +1,6 @@
 import pytest
-from users.forms import UserEmailForm
+from users.forms import CustomSetPasswordForm, UserEmailForm
+from users.tests.factories import UserFactory
 
 
 @pytest.mark.django_db
@@ -15,6 +16,11 @@ def test_invalid_email():
     assert "email" in form.errors
 
 
+@pytest.mark.django_db
 def test_widget_class():
     form = UserEmailForm()
     assert "form-control" in form.fields["email"].widget.attrs["class"]
+    user = UserFactory.create()
+    form = CustomSetPasswordForm(user)
+    assert "form-control" in form.fields["new_password1"].widget.attrs["class"]
+    assert "form-control" in form.fields["new_password2"].widget.attrs["class"]
