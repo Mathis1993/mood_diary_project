3ab92c56aa9824b9e02832e3550b3fa5447e0d3e
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-15 19:44:18 +0200

    #15 Implement password forgotten process

--- mood_diary/users/urls.py
+++ mood_diary/users/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 from users import views
+from users.forms import CustomSetPasswordForm
 
 app_name = "users"
 
@@ -19,5 +21,36 @@ urlpatterns = [
         "password_change_done/",
         auth_views.PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"),
         name="password_change_done",
+    ),
+    path(
+        "reset_password/",
+        auth_views.PasswordResetView.as_view(
+            email_template_name="users/password_reset_email.html",
+            from_email=settings.FROM_EMAIL,
+            template_name="users/password_reset.html",
+            success_url=reverse_lazy("users:password_reset_done"),
+        ),
+        name="reset_password",
+    ),
+    path(
+        "reset_password_sent/",
+        auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html",
+            form_class=CustomSetPasswordForm,
+            success_url=reverse_lazy("users:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset_password_complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
 ]
