61cfc49ae5f32363d95be290bdd3f8789bebeee4
Mathis1993 <m_eric01@uni-muenster.de>
2023-08-25 16:38:28 +0200

    Add activity and acitvity category to admin

--- mood_diary/diaries/admin.py
+++ mood_diary/diaries/admin.py
@@ -1,1 +1,5 @@
-# Register your models here.
+from diaries.models import Activity, ActivityCategory
+from django.contrib import admin
+
+admin.site.register(ActivityCategory)
+admin.site.register(Activity)
