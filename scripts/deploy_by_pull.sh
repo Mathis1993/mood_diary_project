e77cc1d57150095b32bd9983aad3c1f0e6e1d667
Mathis1993 <m_eric01@uni-muenster.de>
2023-12-12 16:20:17 +0100

    Update env variables and add script to deploy by pull

---
+++ scripts/deploy_by_pull.sh
@@ -0,0 +1,22 @@
+# Load the .env file
+source .env
+
+# Navigate to the repository directory
+cd "$REPO_DIR" || exit
+
+# Ensure the main branch is checked out
+git checkout main
+
+# Fetch the latest changes from the remote repository
+git fetch
+
+# Check for changes
+if [ -n "$(git diff origin/main)" ]; then
+  # If there are changes, pull the changes
+  git pull
+
+  # Execute the Docker Compose commands
+  docker login -u "$REGISTRY_USER" -p "$REGISTRY_TOKEN" "$REGISTRY_NAME"
+  docker compose pull && docker compose up -d
+  docker compose exec django python manage.py migrate
+fi
