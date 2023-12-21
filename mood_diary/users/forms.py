6338f79c16106eabd17c50685bc990a6ebfdb257
Mathis1993 <m_eric01@uni-muenster.de>
2023-12-21 14:09:17 +0100

    #26 Override password reset save method to make the reset link work after only using hashed email

--- mood_diary/users/forms.py
+++ mood_diary/users/forms.py
@@ -3,7 +3,16 @@ from typing import Generator
 from core.forms import BaseModelForm, FormWithUIClassMixin
 from core.utils import hash_email
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm, SetPasswordForm
+from django.contrib.auth.forms import (
+    PasswordChangeForm,
+    PasswordResetForm,
+    SetPasswordForm,
+    UserModel,
+)
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 User = get_user_model()
 
@@ -55,3 +64,53 @@ class CustomPasswordResetForm(FormWithUIClassMixin, PasswordResetForm):
         )
         [u.__setattr__(User.EMAIL_FIELD, email) for u in active_users]
         return (u for u in active_users if u.has_usable_password())
+
+    def save(
+        self,
+        domain_override=None,
+        subject_template_name="registration/password_reset_subject.txt",
+        email_template_name="registration/password_reset_email.html",
+        use_https=False,
+        token_generator=default_token_generator,
+        from_email=None,
+        request=None,
+        html_email_template_name=None,
+        extra_email_context=None,
+    ):
+        """
+        Generate a one-use only link for resetting password and send it to the
+        user.
+        """
+        email = self.cleaned_data["email"]
+        if not domain_override:
+            current_site = get_current_site(request)
+            site_name = current_site.name
+            domain = current_site.domain
+        else:
+            site_name = domain = domain_override
+        email_field_name = UserModel.get_email_field_name()
+        for user in self.get_users(email):
+            user_email = getattr(user, email_field_name)
+            # User with empty email field for token generation as the user
+            # calling the password reset link later will have an empty
+            # email field as only the hashed email is stored in the database.
+            user_no_email = user
+            user_no_email.email = None
+            context = {
+                "email": user_email,
+                "domain": domain,
+                "site_name": site_name,
+                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                "user": user,
+                "token": token_generator.make_token(user_no_email),
+                "protocol": "https" if use_https else "http",
+                **(extra_email_context or {}),
+            }
+            self.send_mail(
+                subject_template_name,
+                email_template_name,
+                context,
+                from_email,
+                user_email,
+                html_email_template_name=html_email_template_name,
+            )
