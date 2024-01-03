1b9d7521ad28d93d395e26f8cb89653e7e463fa3
Mathis1993 <m_eric01@uni-muenster.de>
2024-01-03 12:34:10 +0100

    Add migrations to seeding script

--- scripts/seed_production_database.sh
+++ scripts/seed_production_database.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+docker compose exec django python manage.py migrate
 docker compose exec django python manage.py shell_plus -c "from mood_diary.seed_database import seed_database_production; seed_database_production()"
